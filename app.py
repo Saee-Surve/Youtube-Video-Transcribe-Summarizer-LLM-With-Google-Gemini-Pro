5ce011b860f153d354ed8edb1c7c56af66cb52fb
Saee Surve <161334162+Saee-Surve@users.noreply.github.com>
2025-10-25 18:52:27 +0530

    Refactor YouTube transcript summarizer functionality

--- app.py
+++ app.py
@@ -43,11 +43,12 @@ if youtube_link:
     video_id = youtube_link.split("=")[1]
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_container_width = True)
 
-if st.button("Get Detailed Notes"):
+if st.button("Get Summarized Notes"):
     transcript_text = extract_transcript_details(youtube_link)
 
     if transcript_text:
         summary = generate_gemini_content(transcript_text, prompt)
-        st.markdown('## Detailed Notes: ')
+        st.markdown('## Summarized Notes: ')
 
         st.write(summary)
+
